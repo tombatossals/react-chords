ef1a05e7609c8fe450aea589951f0d8833ed7463
David Rubert <david.rubert@gmail.com>
2016-09-22 19:04:32 +0200

    initial page

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className='App'>
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
+          <h2>React-Chords</h2>
         </div>
         <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <p>Embed chord notation of your strings instrument inside your react powered web.</p>
         </p>
       </div>
     )
