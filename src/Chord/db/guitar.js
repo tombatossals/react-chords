913f20044c5a92817ddc0873f651204150c50c25
David Rubert <david.rubert@gmail.com>
2016-09-28 14:36:53 +0200

    db

--- src/Chord/db/guitar.js
+++ src/Chord/db/guitar.js
@@ -1,7 +1,98 @@
 export default {
-  'CMajor': {
+  'C': [{
+    name: 'C Major',
     position: 0,
     strings: [ 0, 1, 0, 2, 3, -1 ],
     bars: []
-  }
+  },
+  {
+    name: 'C7',
+    position: 0,
+    strings: [ 0, 1, 3, 2, 3, -1 ],
+    bars: []
+  }],
+  'D': [{
+    name: 'D Major',
+    position: 0,
+    strings: [ 2, 3, 2, 0, -1, -1 ],
+    bars: []
+  },
+  {
+    name: 'D Minor',
+    position: 0,
+    strings: [ 1, 3, 2, 0, -1, -1 ],
+    bars: []
+  },
+  {
+    name: 'D7',
+    position: 0,
+    strings: [ 2, 1, 2, 0, -1, -1 ],
+    bars: []
+  },
+  {
+    name: 'Dm7',
+    position: 0,
+    strings: [ 1, 1, 2, 0, -1, -1 ],
+    bars: []
+  }],
+  'E': [{
+    name: 'E Major',
+    position: 0,
+    strings: [ 0, 0, 1, 2, 2, 0 ],
+    bars: []
+  },
+  {
+    name: 'E Minor',
+    position: 0,
+    strings: [ 0, 0, 0, 2, 2, 0 ],
+    bars: []
+  },
+  {
+    name: 'E7',
+    position: 0,
+    strings: [ 0, 3, 1, 0, 2, 0 ],
+    bars: []
+  },
+  {
+    name: 'Em7',
+    position: 0,
+    strings: [ 0, 3, 0, 2, 2, 0 ],
+    bars: []
+  }],
+  'G': [{
+    name: 'G Major',
+    position: 0,
+    strings: [ 3, 3, 0, 0, 2, 3 ],
+    bars: []
+  },
+  {
+    name: 'G7',
+    position: 0,
+    strings: [ 1, 0, 0, 0, 2, 3 ],
+    bars: []
+  }],
+  'A': [{
+    name: 'A Major',
+    position: 0,
+    strings: [ 0, 2, 2, 2, 0, -1 ],
+    bars: []
+  },
+  {
+    name: 'A Minor',
+    position: 0,
+    strings: [ 0, 1, 2, 2, 0, -1 ],
+    bars: []
+  },
+  {
+    name: 'A7',
+    position: 0,
+    strings: [ 1, 0, 0, 0, 2, 3 ],
+    bars: []
+  },
+  {
+    name: 'Am7',
+    position: 0,
+    strings: [ 0, 1, 0, 2, 0, -1 ],
+    bars: []
+  }]
 }
