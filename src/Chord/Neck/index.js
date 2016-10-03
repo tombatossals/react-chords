17f7dda572e88c214910a8f5237c26efe604bf94
David Rubert <david.rubert@gmail.com>
2016-10-03 18:14:33 +0200

    Better rendering of moved fret chords

--- src/Chord/Neck/index.js
+++ src/Chord/Neck/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './styles.css'
 
-const Neck = ({ withLetters, tunning, firstFret }) => {
+const Neck = ({ withNotesAtTheEnd, tunning, firstFret }) => {
   return <g>
     <path
       className='Neck'
       d='M 0 0 H 50 M 0 12 H 50 M 0 24 H 50 M 0 36 H 50 M 0 48 H 50
          M 0 0 V 48 M 10 0 V 48 M 20 0 V 48 M 30 0 V 48 M 40 0 V 48 M 50 0 V 48' />
-    { firstFret === 0
+    { firstFret === 1
       ? <path className='Nut' d='M 0 0 H 50' />
-      : <text className='FirstFret' x='-5' y='7'>{firstFret}</text> }
-    { withLetters &&
+      : <text className='FirstFret' x='-7' y='7'>{firstFret}fr</text> }
+    { withNotesAtTheEnd &&
       <g>
         { tunning.map((note, index) =>
           <text key={index} className='Note' x={index * 10} y='56'>{note}</text>
@@ -21,14 +21,14 @@ const Neck = ({ withLetters, tunning, firstFret }) => {
 }
 
 Neck.propTypes = {
-  withLetters: React.PropTypes.bool,
+  withNotesAtTheEnd: React.PropTypes.bool,
   tunning: React.PropTypes.array,
   firstFret: React.PropTypes.oneOf([ 0, 1, 2, 3, 4, 5 ])
 }
 
 Neck.defaultProps = {
-  withLetters: false,
-  firstFret: 0
+  withNotesAtTheEnd: false,
+  firstFret: 1
 }
 
 export default Neck
--- src/Chord/index.js
+++ src/Chord/index.js
@@ -5,23 +5,32 @@ import Barre from './Barre'
 import './styles.css'
 
 const Chord = ({ chord, tunning }) => {
+  const frets = chord.frets.map(fret =>
+    chord.position > 1 ? fret > 0 ? fret - chord.position + 1 : fret : fret
+  )
+
+  const barres = chord.barres.map(barre => ({
+    fret: chord.position > 1 ? barre.fret - chord.position + 1 : barre.fret,
+    strings: barre.strings.slice()
+  }))
+
   return chord
     ? <svg
       className='Chord'
       xmlns='http://www.w3.org/2000/svg'
       preserveAspectRatio='xMinYMin meet'
-      viewBox='0 0 60 100'>
+      viewBox='0 0 64 100'>
       <g className='Title' transform='translate(30, 10)'>
         <text className='Key' x='-7' y='0'>{chord.key}</text>
         <text className='Type' x='0' y='2'>{chord.type}</text>
       </g>
       <g
-        transform='translate(5, 22)'>
-        <Neck withLetters tunning={tunning} firstFret={chord.position} />
-        {chord.barres.map((barre, index) => {
+        transform='translate(7, 22)'>
+        <Neck withNotesAtTheEnd tunning={tunning} firstFret={chord.position} />
+        {barres.map((barre, index) => {
           return barre && <Barre key={index} fret={barre.fret} strings={barre.strings} />
         })}
-        { chord.frets.map((fret, string) => (
+        { frets.map((fret, string) => (
           <Dot key={string} string={string + 1} fret={fret} finger={chord.fingers && chord.fingers[string]} />
         ))}
       </g>
