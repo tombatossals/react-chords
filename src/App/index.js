14d33375c7cf81b7a3bd9ccb4423f6d1ae1e31cf
David Rubert <david.rubert@gmail.com>
2016-10-15 16:09:56 +0200

    All guitar chords rendered

--- src/App/index.js
+++ src/App/index.js
@@ -26,10 +26,10 @@ const App = () => {
           <Match exactly pattern='/react-chords/:instrument' component={Keys} />
           <Match exactly pattern='/react-chords/:instrument/chords/:key([EFGABCD])' component={Suffixes} />
           <Match exactly pattern='/react-chords/:instrument/chords/:key([EFGABCD])/:suffix' component={Variations} />
-          <Match exactly pattern='/react-chords/:instrument/chords/:key([D]sharp)' component={Suffixes} />
-          <Match exactly pattern='/react-chords/:instrument/chords/:key([E]b)' component={Suffixes} />
-          <Match exactly pattern='/react-chords/:instrument/chords/:key([D]sharp)/:suffix' component={Variations} />
-          <Match exactly pattern='/react-chords/:instrument/chords/:key([E]b)/:suffix' component={Variations} />
+          <Match exactly pattern='/react-chords/:instrument/chords/:key([CF]sharp)' component={Suffixes} />
+          <Match exactly pattern='/react-chords/:instrument/chords/:key([EAB]b)' component={Suffixes} />
+          <Match exactly pattern='/react-chords/:instrument/chords/:key([DF]sharp)/:suffix' component={Variations} />
+          <Match exactly pattern='/react-chords/:instrument/chords/:key([EAB]b)/:suffix' component={Variations} />
           <Match exactly pattern='/react-chords' render={() => <Redirect to='/react-chords/guitar' />} />
           <Miss component={NotFound} />
         </div>
