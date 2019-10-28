6ca0b0ee206df8f0e406437e14f45518c1ea5477
David Rubert <david.rubert@gmail.com>
2019-10-28 07:46:28 +0100

    fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Work Sans", "ars-maquette-web"],
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
