84a400b76bad181ad563cac3de850250f39a99cb
Claudemir Casa <claudemir.casa@governor.ltd>
2025-10-15 11:52:18 -0300

    test new build

--- my-chords-tester/src/index.js
+++ my-chords-tester/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
